e9f34951f16c973d57a15c8dc7fbce764be40d22
zhangXiaoLong <744823391@qq.com>
2024-02-01 10:25:14 +0800

    feat(api): 添加获取网页截图的api

--- src/pages/api/getPagePic.ts
+++ src/pages/api/getPagePic.ts
@@ -5,35 +5,21 @@ import puppeteer from 'puppeteer-core';
 import handleRes from '@/pages/api/untils/handleRes';
 
 export default async function getPagePic(req: NextApiRequest, res: NextApiResponse) {
+  const { resSuccess } = handleRes(res);
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({
     executablePath: path.resolve(process.cwd(), 'chrome.exe'),
+    defaultViewport: {
+      width: 1600,
+      height: 937,
+    },
   });
   const page = await browser.newPage();
-
-  // Navigate the page to a URL
-  await page.goto('https://developer.chrome.com/');
-
-  // Set screen size
-  await page.setViewport({ width: 1080, height: 1024 });
-
-  // Type into search box
-  await page.type('.search-box__input', 'automate beyond recorder');
-
-  // Wait and click on first result
-  const searchResultSelector = '.search-box__link';
-  await page.waitForSelector(searchResultSelector);
-  await page.click(searchResultSelector);
-
-  // Locate the full title with a unique string
-  const textSelector = await page.waitForSelector('text/Customize and automate');
-  const fullTitle = await textSelector?.evaluate((el) => el.textContent);
-
-  // Print the full title
-  console.log('The title of this blog post is "%s".', fullTitle);
-
+  await page.goto('https://www.jianshu.com');
+  const picSource = await page.screenshot({ fullPage: true });
+  console.log(picSource);
   await browser.close();
-  const { resSuccess } = handleRes(res);
+
   resSuccess({ msg: 'Hello World' });
 }
 
